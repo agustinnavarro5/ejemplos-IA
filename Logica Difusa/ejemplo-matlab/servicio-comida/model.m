6fcceda10986692120baa649f24bb0f610f53b54
DESKTOP-96VC9JK\usuario <agustinavarro5@gmail.com>
2021-08-10 22:04:12 -0300

    First commit

--- Logica Difusa/ejemplo-matlab/servicio-comida/model.m
+++
@@ -1,9 +0,0 @@
-fuzzyLogicDesigner('tipper_demo');
-fprintf('Presione Enter para continuar \n');
-pause;
-
-out = readfis('tipper_demo');
-result = evalfis([3 7], out);
-
-fprintf('Resultado \n');
-disp(result);
